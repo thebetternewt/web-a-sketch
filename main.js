4994cd1bc36b7fbc0834868504f283a7a951092c
Chris Eady <chris.eady1@gmail.com>
2015-12-10 17:00:59 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	
+	//set default length of grid side(s)
+	numSides = 16;
+
+	drawGrid();
+});
+
+function drawGrid() {
+
+	//create grid rows
+	for (i=0;i<numSides;i++){
+		$(".wrapper").append($("<ul class='row'></ul>"));
+	}
+
+	//create grid tiles
+	for (i=0;i<numSides;i++){
+		$(".wrapper").find(".row").append($("<div class='block'/>"));
+		// $(".block").css("display", "inline-block");
+	}
+
+	//highlight
+	$(".block").mouseenter(function(){
+		$(this).addClass('highlighted');
+	});
+
+	blockWidth = (+$(".wrapper").width()) / numSides;
+	console.log(blockWidth);
+	$('.block').css({
+		height:blockWidth,
+		width:blockWidth
+	});
+	$('.wrapper > .row').css({
+		height:blockWidth
+	});
+}
+
+function newCanvas() {
+	numSides = +prompt("How many squares per side should I make your new grid?");
+
+	$('.wrapper > .row').remove();
+
+	drawGrid();
+}
